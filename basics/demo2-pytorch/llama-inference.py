4162dcc6fec56b4f68842f26dac9f337490a2bac
Hisaki Ohara <160202115+hisohara@users.noreply.github.com>
2025-11-26 18:06:29 +0900

    Update llama-inference for cuda.is_available

--- basics/demo2-pytorch/llama-inference.py
+++ basics/demo2-pytorch/llama-inference.py
@@ -7,7 +7,7 @@ pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
     model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
+    device_map={"": "cuda:0"} if torch.cuda.is_available() else {"": "cpu"},
 )
 query = "Explain the concept of AI."
 messages = [
